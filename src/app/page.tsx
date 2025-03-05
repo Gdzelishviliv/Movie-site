9eb4cc325d4e56221bcd1c19b88b920a28b97194
Nanno <gdzelishviliv@gmail.com>
2025-03-05 15:47:52 +0400

    fix hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import { auth } from "./config/firebase";
 import MainLayout from "@/app/layouts/mainLayout/MainLayout";
 import useFetch from "@/app/utils/useFetch";
 import Image from "next/image";
-import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
+import { EffectCoverflow, Navigation, Autoplay } from "swiper/modules";
 import "./globals.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/scrollbar";
+import "swiper/css/effect-coverflow";
 import Recomendations from "./components/recomended/Recomendations";
 import Effect from "./layouts/enteranceAnimation/enteranceAnimation";
 import CustomCursor from "@/app/components/customCursor/customCursor";
@@ -20,7 +21,6 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    console.log(auth.currentUser);
     if (!auth.currentUser) {
       router.push("/pages/login");
     }
@@ -37,69 +37,60 @@ export default function Home() {
   if (error) {
     return <div>Error fetching movies</div>;
   }
+
   if (!data || !data.results) {
     return <div>No data available.</div>;
   }
 
   const middleSlideIndex = Math.floor(data.results.length / 2);
 
-  {
-    return (
-      <main>
-        <Effect/>
-        <CustomCursor/>
-        <MainLayout>
-          <input
-              className="search-input border-white focus:border-[#FC4747] focus:ring-0 transition-colors duration-700"
-              type="search"
-              name="search"
-              id="search"
-              placeholder="Search for movies or TV series"
-          />
-          <h2 className="text-white text-xl px-4 mt-6">Trending</h2>
-          <Swiper
-              effect={"coverflow"}
-              navigation={true}
-              centeredSlides={true}
-              slidesPerView={"auto"}
-            initialSlide={middleSlideIndex}
-            coverflowEffect={{
-              rotate: 15,
-              stretch: -20,
-              depth: 100,
-              modifier: 1,
-              slideShadows: true,
-            }}
-            modules={[Navigation, EffectCoverflow]}
-            className="mySwiper"
-          >
-            <div className="flex">
-              {data.results.map((movie: any) => (
-                <SwiperSlide className="custom-slide" key={movie.id}>
-                  <span className="movie-name" key={movie.id}>
-                    {movie.title}
-                  </span>
-                  <Image
-                    className="rounded-lg"
-                    src={`https://image.tmdb.org/t/p/original${
-                      movie ? movie.poster_path : ""
-                    }`}
-                    alt="movie-image"
-                    width={240}
-                    height={140}
-                  ></Image>
-                </SwiperSlide>
-              ))}
-            </div>
-          </Swiper>
-          <HowerWrapper>
-            <h1 className="hover:text-white z-50 text-[#FC4747] duration-700 text-[18px] lg:text-[20px] xl:text-[22px] absolute">Recommended
-              for you</h1>
-          </HowerWrapper>
-
-          <Recomendations />
-        </MainLayout>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <Effect />
+      <CustomCursor />
+      <MainLayout>
+        <input
+          className="search-input border-white focus:border-[#FC4747] focus:ring-0 transition-colors duration-700"
+          type="search"
+          name="search"
+          id="search"
+          placeholder="Search for movies or TV series"
+        />
+        <HowerWrapper>
+          <h2 className="text-xl px-4 mt-6 hover:text-white pl-5 text-[#FC4747] duration-700 text-[18px] lg:text-[20px] xl:text-[22px] relative transition-all hover:translate-y-0 hover:z-50">Trending</h2>
+        </HowerWrapper>
+        <Swiper
+          effect="slide"
+          navigation={true}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          centeredSlides={true}
+          slidesPerView="auto"
+          initialSlide={middleSlideIndex}
+          modules={[Navigation, EffectCoverflow, Autoplay]}
+          className="mySwiper"
+        >
+          {data.results.map((movie) => (
+            <SwiperSlide className="custom-slide" key={movie.id}>
+              <span className="movie-name">{movie.title}</span>
+              <Image
+                className="rounded-lg"
+                src={`https://image.tmdb.org/t/p/original${
+                  movie ? movie.poster_path : ""
+                }`}
+                alt="movie-image"
+                width={240}
+                height={140}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <HowerWrapper>
+          <h1 className="hover:text-white pl-4 text-[#FC4747] duration-700 text-[18px] lg:text-[20px] xl:text-[22px] relative transition-all hover:translate-y-0 hover:z-50">
+            Recommended for you
+          </h1>
+        </HowerWrapper>
+        <Recomendations />
+      </MainLayout>
+    </main>
+  );
 }
